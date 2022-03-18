2eb79fe9a06f429444327b9b18a08b12458072a7
Dylan Solorzano <dylan_solorzano@hotmail.com>
2022-03-18 03:11:05 -0300

    guardado

---
+++ Javascript.js
@@ -0,0 +1,36 @@
+
+/*EJEMPLO DE SWITCH
+
+let Genero ="Country";
+
+**********DEFINIMOS LA VARIABLE
+Y CON SWITCH HACEMOS QUE REVISE LA VARIABLE PARA TERMINAR EJECUTANDO LO QUE PEDIMOS**********
+
+switch (Genero) {
+    case "Trap":
+        frase = "808 Bass"
+        break;
+
+    case "Rock":
+        frase ="Overdrive"
+        break;
+
+    default:
+       frase ="Nada que decir"
+        break;
+} 
+
+**********OTRA FORMA DE HACER UNA ESPECIE DE SWITCH ES CON IF Y ELSE**********
+
+if(Genero === "Rock"){
+    frase="Overdrive";
+}
+else if(Genero==="Trap"){
+    frase="808 Bass";
+}
+else if(Genero==="Country"){
+    frase="Vida de campo";
+}
+
+alert(frase);
+*/
